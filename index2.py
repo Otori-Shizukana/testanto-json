44f7da687c7cf387455678cd13027d22f99e4c89
Otori-Shizukana <tdctacilo7@gmail.com>
2024-07-12 14:35:25 -0300

    Teste de JSON

---
+++ index2.py
@@ -0,0 +1,20 @@
+invoice = {
+    "nome": "João",
+    "idade": 33,
+    "compras": {
+        0: ['Teclado Mecânico', 50.00],
+        1: ['Mouse gamer', 90.00],
+        2: ['Monitor 25 polegadas 144hz', 1100.00]
+    }
+}
+
+  
+def geratorInvoice(invoice):
+    print(f'O comprador é: {invoice["nome"]}')
+    print(f'idade: {invoice["idade"]}')
+    print("--------------------------")
+    for index in invoice["compras"]:
+        (produto, valor) = invoice["compras"][index]
+        print(f"Comprou: {produto}, R${valor:.2f}")
+
+geratorInvoice(invoice)
